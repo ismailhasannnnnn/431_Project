cd5f7f2a69d10ea8049775fc0ff42b8c038fa2d5
Ismail Hasan <ismailhasan0132@gmail.com>
2023-05-16 08:18:21 -0700

    read receipts implemented

--- conversation-view.php
+++ conversation-view.php
@@ -7,12 +7,39 @@ function getConversation(): void
         $mysqli = require __DIR__ . "/database.php";
         $email = $_GET['email'];
         $user_email = $_SESSION['user_email'];
-        $fromQuery = "SELECT `from`, `to`, content, datetime FROM messages WHERE `from`='$email' AND `to`='$user_email'";
-        $toQuery = "SELECT `from`, `to`, content, datetime FROM messages WHERE `to`='$email' AND `from`='$user_email'";
+        $fromQuery = "SELECT `from`, `to`, content, datetime, `read`, time_read FROM messages WHERE `from`='$email' AND `to`='$user_email'";
+        $toQuery = "SELECT `from`, `to`, content, datetime, `read`, time_read FROM messages WHERE `to`='$email' AND `from`='$user_email'";
 
         // Combine the two queries into a single query
         $query = "$fromQuery UNION $toQuery ORDER BY datetime ASC";
         $result = $mysqli->query($query);
+
+        $lastMessageQuery = "SELECT m1.`from`, m1.content, m1.datetime, m1.`read`, m1.time_read FROM messages m1
+                            JOIN (
+                                SELECT `from`, MAX(datetime) as max_datetime
+                                FROM messages
+                                WHERE `to` = '$email' AND `from`='$user_email'
+                                GROUP BY `from`
+                                )
+                            m2 ON m1.`from` = m2.`from` AND m1.datetime = m2.max_datetime";
+        $lastMessageResult = mysqli_query($mysqli, $lastMessageQuery);
+        $lastMessage = mysqli_fetch_array($lastMessageResult)[1];
+
+        $lastRecipientMessageQuery = "SELECT m1.`from`, m1.content, m1.datetime, m1.`read`, m1.time_read FROM messages m1
+                            JOIN (
+                                SELECT `from`, MAX(datetime) as max_datetime
+                                FROM messages
+                                WHERE `to` = '$user_email' AND `from`='$email'
+                                GROUP BY `from`
+                                )
+                            m2 ON m1.`from` = m2.`from` AND m1.datetime = m2.max_datetime";
+        $lastRecipientMessageResult = mysqli_query($mysqli, $lastRecipientMessageQuery);
+        $lastRecipientMessage = mysqli_fetch_array($lastRecipientMessageResult)[1];
+
+        $dateTimeVariable = date('Y-m-d H:i:s');
+        $updateReadReceiptQuery = "UPDATE messages SET `read` = true, time_read='$dateTimeVariable' 
+                                    WHERE content='$lastRecipientMessage' AND `to` = '$user_email' AND `from`='$email'";
+        $mysqli->query($updateReadReceiptQuery);
 
         // CSS styles for the conversation
         echo '<style>
@@ -68,17 +95,6 @@ function getConversation(): void
                 }
             </style>';
 
-        $lastMessageQuery = "SELECT m1.`from`, m1.content, m1.datetime FROM messages m1
-                                                   JOIN (
-    SELECT `from`, MAX(datetime) as max_datetime
-    FROM messages
-    WHERE `to` = '$email' AND `from`='$user_email'
-    GROUP BY `from`
-)
-    m2 ON m1.`from` = m2.`from` AND m1.datetime = m2.max_datetime";
-        $lastMessageResult = mysqli_query($mysqli, $lastMessageQuery);
-        $lastMessage = mysqli_fetch_array($lastMessageResult)[1];
-
         // Start the conversation container
         echo '<div class="conversation">';
         echo '<div>';
@@ -91,13 +107,22 @@ function getConversation(): void
             // Determine the CSS class based on the message sender
             $cssClass = ($sender === $email) ? 'recipient-message' : 'sender-message';
             $cssTimestamp = ($sender === $email) ? 'timestamp-recipient' : 'timestamp-sender';
+            $readReceipt = ($row['read'] == 1) ? 'Read' : 'Delivered';
+
 
             // Display the message with appropriate CSS class
             echo '<div class="message-container">';
             echo '<div class="' . $cssClass . '">' . $message . '</div>';
-            echo '<div class="' . $cssTimestamp . '">' . $timestamp . '</div>';
             if ($message == $lastMessage) {
-                echo '<div class="' . $cssTimestamp . '">Delivered</div>';
+                if($readReceipt === 'Read') {
+                    $readTimestampQuery = "SELECT time_read FROM messages WHERE content='$lastMessage' AND datetime='$timestamp'";
+                    $readTimestampResult = mysqli_query($mysqli, $readTimestampQuery);
+                    $timestamp = mysqli_fetch_array($readTimestampResult)[0];
+                }
+
+                echo '<div class="' . $cssTimestamp . '">' . $readReceipt . ' ' . $timestamp . '</div>';
+            } else {
+                echo '<div class="' . $cssTimestamp . '">' . $timestamp . '</div>';
             }
             echo '</div>';
         }
@@ -140,37 +165,39 @@ function getConversation(): void
 </nav>
 
 <div class="container main-card">
-    <?php if(isset($_GET['email'])): ?>
+    <?php if (isset($_GET['email'])): ?>
 
-    <h1>
-        <?php
+        <h1>
+            <?php
             echo $_GET['email'];
-        ?>
-    </h1>
+            ?>
+        </h1>
 
     <?php
-        getConversation();
+    getConversation();
     ?>
 
-    <form class="searchbar" id="searchbarForm" action="send-message.php?" method="post" onsubmit="changeAction()">
-        <input type="text"  name="sendMessage" id="sendMessage">
-        <button style="display:flex; align-items:center; justify-content:center;"><i class="material-icons">arrow</i></button>
-    </form>
+        <form class="searchbar" id="searchbarForm" action="send-message.php?" method="post" onsubmit="changeAction()">
+            <input type="text" name="sendMessage" id="sendMessage">
+            <button style="display:flex; align-items:center; justify-content:center;"><i
+                    class="material-icons">arrow</i></button>
+        </form>
 
-    <script>
-        document.getElementById("sendMessage").focus();
-        function changeAction() {
-            const url = window.location.search;
-            const params = new URLSearchParams(url);
-            const email = params.get('email');
-            document.getElementById("searchbarForm").action = "send-message.php?email=" + email;
-        }
-    </script>
+        <script>
+            document.getElementById("sendMessage").focus();
+
+            function changeAction() {
+                const url = window.location.search;
+                const params = new URLSearchParams(url);
+                const email = params.get('email');
+                document.getElementById("searchbarForm").action = "send-message.php?email=" + email;
+            }
+        </script>
 
 
     <?php else: ?>
 
-    <h1>How did you get here?</h1>
+        <h1>How did you get here?</h1>
 
     <?php endif ?>
 
