185825f5f395e67c56a659977e83cc9e2d829c7f
Ismail Hasan <ismailhasan0132@gmail.com>
2023-05-16 08:38:26 -0700

    binded message params for more security and to allow apostrophes and quotes

--- send-message.php
+++ send-message.php
@@ -6,8 +6,32 @@ if(isset($_POST['sendMessage'])) {
     $user_email = $_SESSION['user_email'];
     $message = $_POST['sendMessage'];
     $dateTimeVariable = date('Y-m-d H:i:s');
-    $query = "INSERT INTO messages VALUES ('$toEmail', '$user_email', '$message', '$dateTimeVariable', false, null)";
-    $result = mysqli_query($mysqli, $query);
+    $falseVal = 0;
+    $nullVal = null;
+//    $query = "INSERT INTO messages VALUES ('$toEmail', '$user_email', '$message', '$dateTimeVariable', false, null)";
+    $query = "INSERT INTO messages (`to`, `from`, content, datetime, `read`, time_read) VALUES (?,?,?,?,?,?)";
+    $stmt = $mysqli->stmt_init();
+    if(!$stmt->prepare($query)){
+        die("SQL error". $mysqli->error);
+    }
+
+    if(!$stmt->bind_param("ssssss",
+        $toEmail,
+        $user_email,
+        $message,
+        $dateTimeVariable,
+        $falseVal,
+        $nullVal)){
+        die("Binding parameters failed:" . $stmt->error);
+    }
+
+    try {
+        $stmt->execute();
+    } catch (Exception $e) {
+        echo "Errors executing statement: " . $e->getMessage();
+    }
+
+//    $result = mysqli_query($mysqli, $query);
     header("Location: conversation-view.php?email=$toEmail");
     exit();
 }
